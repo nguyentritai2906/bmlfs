59c268a91ddd67d688d0b5476207ad54fd6a5b65
nguyentritai2906 <nguyentritai2906@gmail.com>
2021-04-17 18:46:35 +0700

    Linear regression model

---
+++ LinearRegression.py
@@ -0,0 +1,99 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from utils.regression import train_test_split, mean_squared_error
+from sklearn.datasets import make_regression
+
+
+class LinearRegression(object):
+    """ Linear regression model. Models the relationship between a scalar dependent variable y and the independent
+    variables X.
+    Parameters:
+    -----------
+    n_iterations: int
+        The number of training iterations the algorithm will tune the weights for.
+    learning_rate: float
+        The step length that will be used when updating the weights.
+    """
+
+    def __init__(self, n_iterations=100, learning_rate=0.001):
+        self.n_iterations = n_iterations
+        self.learning_rate = learning_rate
+
+    def initialize_weights(self, n_features):
+        """ Initialize weights randomly using Xavier Initialization method [-1/N, 1/N] """
+        limit = 1 / math.sqrt(n_features)
+        self.w = np.random.uniform(-limit, limit, (n_features, ))
+
+    def fit(self, X, y):
+        # Insert constant ones for bias weights
+        X = np.insert(X, 0, 1, axis=1)
+        self.training_errors = []
+        self.initialize_weights(n_features=X.shape[1])
+
+        # Do gradient descent for n_iterations
+        for _ in range(self.n_iterations):
+            y_pred = X.dot(self.w)
+            # Calculate l2 loss
+            mse = np.mean(0.5 * (y - y_pred)**2)
+            self.training_errors.append(mse)
+            # Gradient of l2 loss w.r.t w
+            grad_w = -(y - y_pred).dot(X)
+            # Update the weights
+            self.w -= self.learning_rate * grad_w
+
+    def predict(self, X):
+        # Insert constant ones for bias weights
+        X = np.insert(X, 0, 1, axis=1)
+        y_pred = X.dot(self.w)
+        return y_pred
+
+
+def main():
+
+    X, y = make_regression(n_samples=100, n_features=1, noise=20)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+
+    model = LinearRegression(n_iterations=100)
+
+    model.fit(X_train, y_train)
+
+    # Training error plot
+    n = len(model.training_errors)
+    training, = plt.plot(range(n),
+                         model.training_errors,
+                         label="Training Error")
+    plt.legend(handles=[training])
+    plt.title("Error Plot")
+    plt.ylabel('Mean Squared Error')
+    plt.xlabel('Iterations')
+    plt.show()
+
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    print("Mean squared error: %s" % (mse))
+
+    y_pred_line = model.predict(X)
+
+    # Color map
+    cmap = plt.get_cmap('viridis')
+
+    # Plot the results
+    m1 = plt.scatter(366 * X_train, y_train, color=cmap(0.9), s=10)
+    m2 = plt.scatter(366 * X_test, y_test, color=cmap(0.5), s=10)
+    plt.plot(366 * X,
+             y_pred_line,
+             color='black',
+             linewidth=2,
+             label="Prediction")
+    plt.suptitle("Linear Regression")
+    plt.title("MSE: %.2f" % mse, fontsize=10)
+    plt.xlabel('Day')
+    plt.ylabel('Temperature in Celcius')
+    plt.legend((m1, m2), ("Training data", "Test data"), loc='lower right')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
