b41e54856bd6e228d5369ac18e618b06d689a366
nguyentritai2906 <nguyentritai2906@gmail.com>
2021-04-24 19:59:26 +0700

    Fix MSE equation, add commandline arg

--- utils/regression.py
+++ utils/regression.py
@@ -16,7 +16,7 @@ def train_test_split(X, y, test_size=0.5, shuffle=True, seed=None):
         X, y = shuffle_data(X, y, seed)
     # Split the training data from test data in the ratio specified in
     # test_size
-    split_i = int(len(y) * test_size)
+    split_i = int(len(y) * (1 - test_size))
     X_train, X_test = X[:split_i], X[split_i:]
     y_train, y_test = y[:split_i], y[split_i:]
 
